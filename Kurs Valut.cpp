8e008828d6ef61328ded127bb643a7197cfc0346
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2025-07-15 20:35:42 +0000

    Update Kurs Valut.cpp

--- Kurs Valut.cpp
+++ Kurs Valut.cpp
@@ -31,7 +31,7 @@ int main() {
 	// Алгоритм из суммы в грн:
 
 	// Задержка
-	Sleep(1500);
+	Sleep(1500); // небезопасно
 	cout << "Ваша сумма " << number_dolar << " долларов = " << number_dolar * number_grn << " в грн." << endl;
 
 
